3d33ff02e70aa9e17d05c8355dd36f97ea56f038
mp0x6 <mp0x6@users.noreply.github.com>
2017-12-06 18:35:05 +0100

    add doublekiller to repository

--- killdoubles.sh
+++ killdoubles.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+rm -r "./.tmp"
 echo "Usage: ./killdoubles.sh NAMEOFPDF.pdf"
 FILENAME=$1
 mkdir "./.tmp"
 cd "./.tmp"
+
 gs -o doube%04d.pdf -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -sDEVICE=pdfwrite "../$FILENAME"
 gs -o doube%04d.bmp -sDEVICE=bmp256 "../$FILENAME"
-pdfunite `sha256sum *.bmp | cut -d " " -f 1  | sort  | uniq -u  | grep -f - `sha256sum doube*.bmp`  | cut -d " " -f 3 | cut -d "." -f 1 | awk '{print $1".pdf"}'` "../$FILENAME"_optimized
+shasum *.bmp > sha.txt
+pdfunite `shasum *.bmp | cut -d " " -f 1  | sort | guniq | ggrep -f - sha.txt | guniq -w 40 | cut -d " " -f 3 | cut -d "." -f 1 | awk '{print $1".pdf"}'` "../$FILENAME"_optimized.pdf
+#        ^ evaluate the command in backticks and sending it as a string to pdfunite
+#          ^ generate hashes of every bmp file in the folder
+#                           ^ search for every space as a delimiter; in the case of the sha1sum-command it's the sha1 hash of the file
+#                                               ^ sort every sha1-hash alphabetically
+#                                                       ^ only show unique hashes (so double hashes are omitted)
+#                                                                         
+cd ..
+#rm -r "./.tmp"
