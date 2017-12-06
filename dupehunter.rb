eae4f88f161ba6214e4d82cbc32c38cf5e9c033d
mp0x6 <mp0x6@users.noreply.github.com>
2017-12-06 18:31:45 +0100

    wrote a tool for ruby to omit duplicate bmp files

---
+++ dupehunter.rb
@@ -0,0 +1,24 @@
+require 'digest'
+require 'pp'
+
+foldername = ARGV[0]
+# foldername = './.tmp'
+files_in_folder = Dir.entries('./.tmp').reject { |f| File.directory? f}
+files_in_folder = files_in_folder.select do |elem|
+  File.extname(elem) == '.bmp'
+end
+
+hashed_files = Array.new
+files_in_folder.each() { |file|
+  filename = foldername + '/' + file
+  hash_of_file = Digest::SHA256.file filename
+  hash_of_file_hex = hash_of_file.hexdigest
+  filename_hash_pair = [filename, hash_of_file_hex]
+  hashed_files << filename_hash_pair
+}
+
+files_without_dupes = hashed_files.uniq {|s| s[1]}
+
+files_without_dupes.each { |file|
+  puts file.first
+}
